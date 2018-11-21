5a5d885fa01082c792a8d25bf17342003eeca7d4
bozhang2018 <35781048+bozhang2018@users.noreply.github.com>
2018-11-20 20:31:32 -0500

    Update scripts.js

--- patient_v1/scripts.js
+++ patient_v1/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'http://FHIR.server/patient/123', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
